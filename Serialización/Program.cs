c45559843dd475e156c9fd957a9ebc28a3cd8c37
Sebastián Altmann <saaltmann@miuandes.cl>
2020-05-07 09:10:49 -0400

    Project File created.

---
+++ Serialización/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Serialización
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

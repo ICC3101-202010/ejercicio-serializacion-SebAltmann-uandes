375a6ef65886c16d07622111d2ac917609c0ca17
Sebastián Altmann <saaltmann@miuandes.cl>
2020-05-07 09:21:22 -0400

    Added prototype menu

--- Serialización/Program.cs
+++ Serialización/Program.cs
@@ -6,7 +6,35 @@ namespace Serialización
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while (true) {
+                String response;
+                Console.WriteLine("Agregar Personas: {A}");
+                Console.WriteLine("Ver Personas: {V}");
+                Console.WriteLine("Cargar Personas: {C}");
+                Console.WriteLine("Salir: {S}\n");
+                response = Console.ReadLine();
+
+                if (response == "A")
+                {
+                    return;
+                }
+
+                if (response == "V")
+                {
+                    return;
+                }
+
+                if (response == "C")
+                {
+                    return;
+                }
+
+                if (response == "S")
+                {
+                    return;
+                }
+            }
+
         }
     }
 }

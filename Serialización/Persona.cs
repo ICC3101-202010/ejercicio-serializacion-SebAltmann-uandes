0b57fb42204e4c899e807019b6c87603b33a73bc
Sebastián Altmann <saaltmann@miuandes.cl>
2020-05-07 10:07:23 -0400

    Serialized class: Persona. Added functionality to the menu

--- Serialización/Persona.cs
+++ Serialización/Persona.cs
@@ -1,10 +1,20 @@
 ﻿using System;
+using System.Runtime.Serialization;
+
 namespace Serialización
 {
+    [Serializable]
     public class Persona
     {
-        public Persona()
+        public string nombre;
+        public string apellido;
+        public int edad;
+
+        public Persona(String nombre, String apellido, int edad)
         {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.edad = edad;
         }
     }
 }
--- Serialización/Program.cs
+++ Serialización/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Serialización
 {
@@ -6,6 +10,10 @@ namespace Serialización
     {
         public static void Main(string[] args)
         {
+            List<Persona> people = new List<Persona>();
+
+            IFormatter formatter = new BinaryFormatter();
+
             while (true) {
                 String response;
                 Console.WriteLine("Agregar Personas: {A}");
@@ -16,25 +24,55 @@ namespace Serialización
 
                 if (response == "A")
                 {
-                    return;
+                    String new_nombre;
+                    String new_apellido;
+                    String new_edad;
+                    String almacenar;
+                    Console.Write("Nombre de la persona nueva: ");
+                    new_nombre = Console.ReadLine();
+                    Console.Write("Apellido de la persona nueva: ");
+                    new_apellido = Console.ReadLine();
+                    Console.Write("Edad de la persona nueva: ");
+                    new_edad = Console.ReadLine();
+
+                    Persona person = new Persona(new_nombre, new_apellido, Convert.ToInt32(new_edad));
+                    people.Add(person);
+
+                    Console.Write("Desea almacenar esta persona?: {Y/N}");
+                    almacenar = Console.ReadLine();
+
+                    if(almacenar == "Y")
+                    {
+                        Stream stream = new FileStream("Person.bin", FileMode.Create, FileAccess.Write, FileShare.None);
+                        formatter.Serialize(stream, person);
+                        stream.Close();
+                    }
+
+                    almacenar = "x";
                 }
 
                 if (response == "V")
                 {
-                    return;
+                    foreach(Persona person in people)
+                    {
+                        Console.WriteLine(person);
+                    }
                 }
 
                 if (response == "C")
                 {
-                    return;
+                    Stream stream = new FileStream("Person.bin", FileMode.Open, FileAccess.Read, FileShare.Read);
+                    Persona persona = (Persona)formatter.Deserialize(stream);
+                    people.Add(persona);
                 }
 
                 if (response == "S")
                 {
                     return;
                 }
-            }
 
+                response = "x";
+            }
         }
     }
 }
